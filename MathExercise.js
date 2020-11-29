020e7b7a19bcd1740df083d4e2900928be77fd01
vishnu92karthik <vishnumkarthik@gmail.com>
2020-11-29 10:30:32 +0530

    MathOperations

---
+++ MathExercise.js
@@ -0,0 +1,8 @@
+Use the correct Math method to create a random number.
+var r = Math.random();
+Use the correct Math method to return the largest number of 10 and 20.
+var x = Math.max(10, 20);
+Use the correct Math method to round a number to the nearest integer.
+var x = Math.round(5.3);
+Use the correct Math method to get the square root of 9.
+var x = Math.sqrt(9);
