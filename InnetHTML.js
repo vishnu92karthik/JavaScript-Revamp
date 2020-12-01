1fb76a60eefe90499a4c8463386ab944c90a4fa7
vishnu92karthik <vishnumkarthik@gmail.com>
2020-12-01 16:30:53 +0530

    innerHTML

---
+++ InnetHTML.js
@@ -0,0 +1,11 @@
+`<button onClick= 'change()'>Changes text</button>`
+function change(){
+    return document.write('Hello JavaScript');
+    }
+
+    /////////////////////////////////////////
+
+    <button onClick= 'document.getElementById("demo").innerHTML="hello vishnu"'>Changes text</button>
+<p id="demo">
+Hi
+</p>
