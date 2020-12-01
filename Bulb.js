081920bcdb5afadc250e656fa68d80659af97440
vishnu92karthik <vishnumkarthik@gmail.com>
2020-12-01 16:30:53 +0530

    Bulb

---
+++ Bulb.js
@@ -0,0 +1,8 @@
+function turnon(){
+    let image = document.getElementById('img1').src='bulbon.gif';
+    return image;
+}
+function turnoff(){
+    let image = document.getElementById('img1').src='bulboff.gif';
+    return image;
+}
