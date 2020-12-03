7c9dde4d5f36b926a9aee2761eaa30182947f35d
vishnu92karthik <vishnumkarthik@gmail.com>
2020-12-03 16:30:53 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,7 @@
+//document.getElementById('click').style.border = '2px solid red';
+let tn = document.getElementsByTagName('div');
+console.log(tn);
+createElement = document.createElement('p');
+createElement.innerText = 'this is created one';
+tn[0].appendChild(createElement);
+document.scripts
