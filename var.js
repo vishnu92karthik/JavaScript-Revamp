80e7b6502d71b54e91f400708c79a485235af16c
vishnu92karthik <vishnumkarthik@gmail.com>
2020-12-03 16:30:53 +0530

    variables

---
+++ var.js
@@ -0,0 +1,32 @@
+// containers to store data values
+var number = 34;
+var num2 = 56;
+console.log(number+num2)
+ var x = 3;
+  var y =6;
+   var z = x+y;
+   console.log(z);
+   var pi = 3.14;
+var name = "vishnu";
+console.log(pi);
+var carName;
+console.log(carName);
+var carName = 'BMW'
+console.log(carName);
+document.getElementById('demo').innerHTML = carName;
+var name = 'karthik', roll = 26 , bike = 'continental-gt' 
+document.getElementById('demo').innerHTML = bike;
+ var join = 'vishnu has' + "" + 'bike';
+ console.log(join)
+  var join = 'vishnu has' + `` + 'bike' + '5'+ 8+9;
+ console.log(join)
+ var join = 'vishnu has' + `` + 'bike' + 8+9+'5'+ 8+9;
+ console.log(join)
+ var join = 10+22+'vishnu has' + `` + 'bike' + '5'+ 8+9;
+ console.log(join)
+  var $ = 5;
+  var $classicGT5 = 10;
+  document.getElementById('demo').innerHTML = $+$classicGT5;
+  var _lastname = 'karthik';
+  console.log(_lastname)
+  
