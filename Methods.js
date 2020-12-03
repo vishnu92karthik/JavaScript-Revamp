429e8e5efbc14eaec5fd8075ce6b41cebc5cc177
vishnu92karthik <vishnumkarthik@gmail.com>
2020-12-03 17:30:53 +0530

    Methods

---
+++ Methods.js
@@ -0,0 +1,16 @@
+let myarr= ['vishnu','karthik',null,26,true]
+myarr.pop()
+myarr.push('hari')
+myarr.shift()
+myarr.unshift(26);
+myarr.toString();
+myarr.sort()
+console.log(myarr)
+
+let myString = "vishnu is bike rider"
+console.log(myString.length)
+console.log(myString.indexOf('bike'))
+console.log(myString.lastIndexOf('bike'))
+console.log(myString.slice(0,3))
+myString.replace('bike','car');
+console.log(myString)
