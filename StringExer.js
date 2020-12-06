2fd534c2c00cc6965760f7ebb7aab6bd27f8e29f
vishnu92karthik <vishnumkarthik@gmail.com>
2020-12-06 16:50:53 +0530

    String

---
+++ StringExer.js
@@ -0,0 +1,26 @@
+Use the length property to alert the length of txt.
+var txt = "Hello World!";
+var x = txt.length;
+alert(x);
+Use escape characters to alert We are "Vikings".
+var txt = "We are \"Vikings\"";
+alert(txt);
+Concatenate the two strings to alert "Hello World!".
+var str1 = "Hello ";
+var str2 = "World!";
+alert(str1+str2);
+Find the position of the character h in the string txt.
+var txt = "abcdefghijklm";
+var pos = txt.indexOf("h");
+Use the slice method to return the word "bananas".
+var txt = "I can eat bananas all day";
+var x = txt.slice(10,17);
+Use the correct String method to replace the word "Hello" with the word "Welcome".
+var txt = "Hello World";
+txt = txt.replace("Hello", "Welcome");
+Convert the value of txt to upper case.
+var txt = "Hello World";
+txt = txt.toUpperCase();
+Convert the value of txt to lower case.
+var txt = "Hello World";
+txt = txt.toLoweerCase();
