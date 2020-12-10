c4d56e7ca390c1e76cea299b75d9961c654b1365
vishnu92karthik <vishnumkarthik@gmail.com>
2020-12-10 09:50:33 +0530

    Comparision

---
+++ ComparisionExer.js
@@ -0,0 +1,16 @@
+Choose the correct comparison operator to alert true, when x is greater than y.
+x = 10;
+y = 5;
+alert(x > y);
+Choose the correct comparison operator to alert true, when x is equal to y.
+x = 10;
+y = 10;
+alert(x == y);
+Choose the correct comparison operator to alert true, when x is NOT equal to y.
+x = 10;
+y = 5;
+alert(x != y);
+Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+var age = n;
+var voteable = (age <18) ?"Too young" : "Old enough";
+alert(voteable);
