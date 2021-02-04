1afe714f366dd990cb385cd28a62ab989537c0e6
vishnu92karthik <vishnumkarthik@gmail.com>
2021-02-04 15:14:23 +0530

    programizadd

---
+++ Add.js
@@ -0,0 +1,16 @@
+const num1 = parseInt(prompt('Enter first number '));
+ const num2 = parseInt(prompt('Enter second number '));
+ 
+ const sum = num1+num2;
+ document.write('the sum of' +num1 +'and'+num2 +'is'+sum );
+
+ var x = 1.23456;
+ var y = 30;
+ var sum = x+y;
+ var sum1 = (sum).toFixed(2);
+ console.log(sum);
+  console.log(sum1);
+
+  var x = 1.4 
+  var y = 1.2; 
+  var z = Math.round(x+y);
