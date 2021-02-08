d6d0ed7465817333ed1f9c802894c66e66a1b76f
vishnu92karthik <vishnumkarthik@gmail.com>
2021-02-08 15:14:23 +0530

    Swaping

---
+++ Swap.js
@@ -0,0 +1,30 @@
+Using a Temporary Variable
+//JavaScript program to swap two variables
+let a = parseInt(prompt('enter a value: '));
+let b = parseInt(prompt('enter b value: '));
+
+let temp;
+temp = a;
+a=b;
+b=temp;
+console.log(`the value after swapping: ${a}`);
+console.log(`the value after swapping: ${b}`);
+
+// Using es6(ES2015) Destructuring assignment
+let a = parseInt(prompt('enter a value: '));
+ let b = parseInt(prompt('enter b value: '));
+ 
+[a,b]=[b,a];
+ console.log(`the value after swapping: ${a}`);
+ console.log(`the value after swapping: ${b}`);
+
+ //Using Arithmetic Operators
+ let a = parseInt(prompt('enter a value: '));
+ let b = parseInt(prompt('enter b value: '));
+ 
+a= a+b;
+b= a-b;
+a = a-b;
+ console.log(`the value after swapping: ${a}`);
+ console.log(`the value after swapping: ${b}`);
+ 
