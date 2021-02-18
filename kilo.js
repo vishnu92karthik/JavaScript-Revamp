6c30cdea59b79d585433574af12e178e9220fcc5
vishnu92karthik <vishnumkarthik@gmail.com>
2021-02-18 15:14:23 +0530

    kmtomi

---
+++ kilo.js
@@ -0,0 +1,21 @@
+/*We know that 1 kilometer is equal to 0.621371 miles.
+
+So to convert kilometers to miles, you can use the formula:
+
+miles = kilometers * 0.621371*/
+
+let mi = parseInt(prompt('enter a value: '));
+
+let kilometer = mi*0.62137;
+
+console.log(kilometer);
+
+To convert miles to kilometers, you can use formula:
+
+kilometers = miles / factor
+
+let km = parseInt(prompt('enter a value: '));
+ 
+let mi = km/0.62137;
+
+ console.log(mi);
