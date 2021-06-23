c949a44d62912efc7eceb4e72a4bcd53f7da3368
vishnu92karthik <vishnumkarthik@gmail.com>
2021-06-23 20:36:02 +0530

    DateSet

---
+++ DateSet.js
@@ -0,0 +1,28 @@
+const d = new Date;
+d.setTime(20)
+document.write(d);
+
+const y = new Date;
+d.setFullYear(1992)
+document.write(y);
+
+const fy = new Date;
+d.setFullYear(2020, 11, 3);
+document.write(fy);
+
+const M = new Date;
+d.setMonth(11)
+document.write(M);
+
+const dd = new Date;
+d.setDate(20)
+document.write(dd);
+
+const d = new Date();
+d.setDate(d.getDate() + 50);
+
+const d = new Date();
+d.setHours(22);
+
+const d = new Date();
+d.setSeconds(30);
