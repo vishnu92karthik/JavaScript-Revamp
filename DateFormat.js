c76cd5f875de8fccdc97ec4dd3d4150084b1a871
vishnu92karthik <vishnumkarthik@gmail.com>
2021-06-23 19:42:50 +0530

    DateFormat

---
+++ DateFormat.js
@@ -0,0 +1,14 @@
+
+const d = new Date("2021-03-11");
+document.write(d);
+const ISO = new Date("2021-03");
+document.write(ISO);
+
+const ISOY = new Date("2021");
+document.write(ISOY);
+
+const ISOT = new Date("2015-03-25T12:00:00Z");
+document.write(ISOT);
+
+let msec = Date.parse("March 10, 2021");
+document.write(msec);
