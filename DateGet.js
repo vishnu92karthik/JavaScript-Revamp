9f5d3f3a27973e90f38c11fa67144f07c706f02c
vishnu92karthik <vishnumkarthik@gmail.com>
2021-06-23 20:17:05 +0530

    DateGet

---
+++ DateGet.js
@@ -0,0 +1,9 @@
+const d = new Date;
+document.write(d.getTime());` <br>`
+document.write(d.getFullYear());
+document.write(d.getHours());
+document.write(d.getMinutes());
+document.write(d.getSeconds());
+document.write(d.getMilliSeconds());
+document.write(d.getDay());
+document.write(d.getUTCDay());
