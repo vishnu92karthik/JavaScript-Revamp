07fade5c45c4fa42debc40f821012cadad060c54
vishnu92karthik <vishnumkarthik@gmail.com>
2021-06-24 20:55:00 +0530

    ArraysExer

---
+++ ArraExer.js
@@ -0,0 +1,17 @@
+Get the value "Volvo" from the cars array.
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+let x = cars[1];
+
+Change the first item of cars to "Ford".
+
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+cars[0]= "Ford";
+
+Alert the number of items in an array, using the correct Array property.
+
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
